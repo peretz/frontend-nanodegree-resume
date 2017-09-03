f013cc5a6796625fefa37e070b0837dc98ca9429
Peretz <peretzman@gmail.com>
2017-09-03 12:50:03 -0500

    Added a single entry to work experience and education.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -20,11 +20,28 @@ var bio = {
     ]
 }
 
+// Work as dot notation.
+var workExperience = {};
+workExperience.position = "Staff Software Engineer";
+workExperience.employer = "National Instruments";
+workExperience.years = "August 2012 - August 2017";
+workExperience.city = "Austin, TX";
+
+// Education as bracket notation.
+var education = {};
+education["school"] = "Columbia University";
+education["degree"] = "MS in Electrical Engineering";
+education["years"] = "2007 - 2008";
+education["city"] = "New York, NY";
+
+
+// Header
 var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
 var formattedName = HTMLheaderName.replace("%data%", bio.name);
 $("#header").prepend(formattedRole);
 $("#header").prepend(formattedName);
 
+// Contact Information
 var formattedMobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
 var formattedEmail = HTMLemail.replace("%data%", bio.contacts.email);
 var formattedGithub = HTMLgithub.replace("%data%", bio.contacts.github);
@@ -39,9 +56,29 @@ var formattedWelcomeMsg = HTMLwelcomeMsg.replace("%data%", bio.welcomeMsg);
 $("#header").append(formattedPic);
 $("#header").append(formattedWelcomeMsg);
 
+// Skills
 $("#header").append(HTMLskillsStart);
 for (var i = 0; i < bio.skills.length; i++) {
     var formattedSkills = HTMLskills.replace("%data%", bio.skills[i]);
     $("#header").append(formattedSkills);
 }
 
+// Work Experience
+$("#workExperience").append(HTMLworkStart);
+var formattedEmployer = HTMLworkEmployer.replace("%data%", workExperience.employer);
+var formattedTitle = HTMLworkTitle.replace("%data%", workExperience.position);
+var formattedWorkDates = HTMLworkDates.replace("%data%", workExperience.years);
+var formattedWorkLocation = HTMLworkLocation.replace("%data%", workExperience.city);
+$("#workExperience").append(formattedEmployer + formattedTitle);
+$("#workExperience").append(formattedWorkDates);
+$("#workExperience").append(formattedWorkLocation);
+
+// Education
+$("#education").append(HTMLschoolStart);
+var formattedSchool = HTMLschoolName.replace("%data%", education["school"]);
+var formattedDegree = HTMLschoolDegree.replace("%data%", education["degree"]);
+var formattedSchoolDates = HTMLschoolDates.replace("%data%", education["years"]);
+var formattedSchoolLocation = HTMLschoolLocation.replace("%data%", education["city"]);
+$("#education").append(formattedSchool + formattedDegree);
+$("#education").append(formattedSchoolDates);
+$("#education").append(formattedSchoolLocation);
