d71e257c348acc15c99cb7e1b9debfe0ad466732
Peretz <peretzman@gmail.com>
2018-08-20 14:00:47 -0500

    Updated Title, contact info and work experience.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -4,12 +4,12 @@ This is empty on purpose! Your code to build the resume will go here.
 
 var bio = {
     "name": "Jaime Peretzman",
-    "role": "Software Engineer",
+    "role": "Software Engineer - Product Manager",
     "contacts": {
-        "mobile": "914-450-9710",
-        "email": "peretzman@caa.columbia.edu",
+        "mobile": "+52-1-55-8426-0918",
+        "email": "jaime@intermed.com.mx",
         "github": "github.com/peretz",
-        "loc": "Austin, TX"
+        "loc": "Mexico City, Mexico"
     },
     "welcomeMsg": "Software Engineer with experience building scalable software architectures: Experienced with OOD and design patterns, data structures and algorithmic complexity, agile and lean development methodologies, and unit and system testing techniques. Great asset for a project that is scaling up and adding new features.",
     "skills": [
@@ -52,6 +52,19 @@ bio.display = function() {
 var workExperience = {
     "Jobs": 
     [
+        {
+            "employer": "Clínica de Medicina Interdisciplinaria",
+            "position": "Operations and Business Development Manager",
+            "years": "September 2017 - Present",
+            "city": "Mexico City, Mexico",
+            "tasks": 
+            [
+                "Designed and implemented a calendar application, which costs were 90% cheaper than the next proposal.",
+                "Using web-scraping techniques, gathered a database of medical specialists and created several outbound marketing campaigns. These campaigns generated an ROI of 205x.",
+                "Adapted a home mesh network to our business, reducing 75% equipment costs.",
+                "Optimized Google Adwords for an inbound campaign, the ROI was 120x."
+            ]
+        },
         {
             "employer": "National Instruments",
             "position": "Staff Software Engineer",
